d2fefed6f6d07ee19aa536b9fe76fb561ed93472
zahra-moeini <z.sh.moeini1379@gmail.com>
2022-05-30 15:21:49 +0430

    comit3

---
+++ my-app/src/components/Countries.js
@@ -0,0 +1,51 @@
+import { useState, useEffect } from "react";
+import { v4 as uuidv4 } from "uuid";
+
+const url = "https://restcountries.com/v3.1/all";
+
+const Countries = () => {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    const fetchCountryData = async () => {
+      const response = await fetch(url);
+      const countries = await response.json();
+      setCountries(countries);
+      console.log(countries);
+    };
+
+    fetchCountryData();
+  }, []);
+
+  return (
+    <>
+      <section className="grid">
+        {countries.map((country, index) => {
+          const { name, population, region, capital, flags } = country;
+
+          return (
+            <article key={uuidv4()}>
+              <div>
+                <img src={flags.png} alt={name.common} />
+                <div className="details">
+                  <h3>{name.common}</h3>
+                  <h4>
+                    Population: <span>{population}</span>
+                  </h4>
+                  <h4>
+                    Region: <span>{region}</span>
+                  </h4>
+                  <h4>
+                    Capital: <span>{capital}</span>
+                  </h4>
+                </div>
+              </div>
+            </article>
+          );
+        })}
+      </section>
+    </>
+  );
+};
+
+export default Countries;
---
+++ my-app/src/components/Header.js
@@ -0,0 +1,17 @@
+const Header = () => {
+  return (
+    <>
+      <header className="header">
+        <div>
+          <h3>Where in the world?</h3>
+        </div>
+
+        <div>
+          <i className="fas fa-moon"></i>Dark Mode
+        </div>
+      </header>
+    </>
+  );
+};
+
+export default Header;
